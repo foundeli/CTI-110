f628609e61bd4a506217a98ad480005d03e363f9
foundeli <elibai6729@gmail.com>
2024-09-19 11:18:40 -0400

    Add files via upload

---
+++ p1hw1_BaileyElijah.py
@@ -0,0 +1,23 @@
+# Elijah Bailey
+# 9/17/2024
+# P1HW1
+# Input and Output with mathematical expressions
+
+print("------Calculating Exponents------")
+print()
+print()
+
+# Get base value (as an integer) from the user
+base_value = int(input("Enter an integer as a base value: "))
+
+# Get exponent value from user
+exponent = int(input("Enter as integer as the exponent: "))
+
+# Raise the base_value to the exponent
+value = base_value ** exponent
+
+print()
+print()
+
+# Display output to the user
+print(base_value, "raise to the power of", exponent, "is", value, "!!")
