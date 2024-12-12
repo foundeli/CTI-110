e55d8e250b05977ee15d4ae9f79a9f5ecacba865
foundeli <elibai6729@gmail.com>
2024-12-12 14:14:30 -0500

    Add files via upload

---
+++ p2hw2_BaileyElijah.py
@@ -0,0 +1,47 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/baileye3578/Desktop/p2hw2_BaileyElijah.py
+Enter grade for module 1: 65.5
+Enter grade for module 2: 88
+Enter grade for module 3: 78.5
+Enter grade for module 4: 90
+Enter grade for module 5: 61
+Enter grade for module 6: 92
+----------Results-----------
+Lowest Grade : 61.0 
+Highest Grade : 92.0 
+Sum of Grades : 475.0 
+Average : 6 !!
+
+-------------------------------
+>>> 
+========== RESTART: C:/Users/baileye3578/Desktop/p2hw2_BaileyElijah.py =========
+Enter grade for module 1: 65.5
+Enter grade for module 2: 88
+Enter grade for module 3: 78.5
+Enter grade for module 4: 90
+Enter grade for module 5: 61
+Enter grade for module 6: 92
+----------Results-----------
+Lowest Grade : 61.0 
+Highest Grade : 92.0 
+Sum of Grades : 475.0 
+Average : 79.16666666666667 !!
+
+-------------------------------
+>>> 
+========== RESTART: C:/Users/baileye3578/Desktop/p2hw2_BaileyElijah.py =========
+Enter grade for module 1: 65.5
+Enter grade for module 2: 88
+Enter grade for module 3: 78.5
+Enter grade for module 4: 90
+Enter grade for module 5: 61
+Enter grade for module 6: 92
+----------Results-----------
+Lowest Grade : 61.0 
+Highest Grade : 92.0 
+Sum of Grades : 475.0 
+Average : 79.17 !!
+
+-------------------------------
